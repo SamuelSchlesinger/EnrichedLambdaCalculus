c3355cdfebcb4935a50f0051d97d81ac3183a234
Samuel Schlesinger <sgschlesinger@gmail.com>
2016-06-30 15:30:52 -0700

    first commit

---
+++ AST.hs
@@ -0,0 +1,27 @@
+-- A La "The Implementation of Functional Programming Languages"
+-- by SLPJ
+
+module AST where
+
+data Exp
+  = ConstExp Constant
+  | VarExp Variable
+  | App Exp Exp
+  | Lam Pattern Exp
+  | Let Pattern Exp Exp
+  | Letrec [(Pattern, Exp)] Exp
+  | Case Variable [(Pattern, Exp)] deriving Show
+
+data Pattern
+  = ConstPat Constant
+  | VarPat Variable
+  | DataPat Constructor [Pattern] deriving Show
+
+data Constant
+  = NatC Integer deriving Show
+
+data Variable
+  = Bound Int
+  | Free String deriving Show
+
+type Constructor = String
---
+++ Parser.hs
@@ -0,0 +1,80 @@
+-- A La "The Implementation of Functional Programming Languages"
+-- by SLPJ
+
+{-# LANGUAGE RankNTypes #-}
+
+module Parser where
+
+import AST
+import Text.Parsec.Char
+import Text.Parsec
+import Data.Char
+import Control.Monad.Identity
+
+type Parser x = ParsecT String () Identity x
+
+-- | Constants are simply natural numbers.
+constant :: Parser Constant
+constant = many1 digit >>= pure . NatC . \ns -> toInteger $ sum $ zipWith (*) [10 ^ n | n <- [0..]] $ map digitToInt $ reverse ns
+
+-- | A variable is a sequence of lowercase letters
+variable :: Parser Variable
+variable = do { xs <- many1 lower;
+                if xs `elem` ["in", "let", "letrec"] then fail "Nothing good here" else pure $ Free xs }
+
+-- | A constructor begins with a capital letter and ends with lowercase letters
+constructor :: Parser String
+constructor = do { x <- upper; xs <- many lower; pure (x : xs) }
+
+-- | In order to parse patterns, I must be able to parse variables, constants,
+--   constructors, and nested patterns.
+pattern :: Parser Pattern
+pattern = (char '(' *> spaces *> pattern <* spaces <* char ')') <|>
+          (constant >>= pure . ConstPat) <|> 
+          (variable >>= pure . VarPat) <|>
+          (do { c <- constructor; xs <- many (spaces *> pattern <* spaces) ; pure $ DataPat c xs })
+
+-- | In order to parse expressions, we can reuse much of the above:
+--   Constants (ConstExp)
+--   Variables (VarExp)
+--   Patterns (in Lam, Let, Case)
+--   
+--   We also must be able to parse Applications (App), Lambdas (Lam),
+--   Let bindings (Let), Letrec bindings (Letrec), and Case statements.
+expression :: Parser Exp
+expression = undefined
+
+lambda :: Parser Exp
+lambda = do { char '\\';
+              p <- spaces *> pattern <* spaces;
+              char '.';
+              e <- spaces *> expression;
+              pure $ Lam p e }
+
+
+letp :: Parser Exp
+letp = do { string "let";
+            p <- spaces *> pattern <* spaces;
+            char '=';
+            e <- spaces *> expression <* spaces;
+            string "in";
+            e <- spaces *> expression;
+            pure $ Let p e e }
+
+letrec_bindings :: Parser [(Pattern, Exp)]
+letrec_bindings = do { p <- spaces *> pattern <* spaces;
+                       char '=';
+                       e <- spaces *> expression <* spaces;
+                       ((string "in" *> pure [(p, e)]) <|> 
+                       (do { newline; 
+                             ps <- letrec_bindings; 
+                             pure ((p, e) : ps); })) }
+
+letrec :: Parser Exp
+letrec = do { string "letrec";
+              bs <- letrec_bindings;
+              e <- spaces *> expression;
+              pure $ Letrec bs e }
+
+casep :: Parser Exp
+casep = undefined
